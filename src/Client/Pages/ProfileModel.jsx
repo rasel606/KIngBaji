bf3723374768185523347dabb765b97b61d2d29b
rasel606 <arafathosensawon@gmail.com>
2025-05-11 17:44:52 +0600

    189

--- src/Client/Pages/ProfileModel.jsx
+++ src/Client/Pages/ProfileModel.jsx
@@ -45,7 +45,7 @@ export default ({ modalName }) => {
       handelUserDetails(userId);
 
       const response = await axios.post(
-        "http://localhost:5000/api/v1/user_balance",
+        "https://api.kingbaji.live/api/v1/user_balance",
         { userId }
       );
       // console.log(response);
@@ -87,7 +87,7 @@ export default ({ modalName }) => {
   //    setRefreshing(true);
 
   //    try {
-  //      const response = await axios.post("http://localhost:5000/api/v1/user_balance", {userId});
+  //      const response = await axios.post("https://api.kingbaji.live/api/v1/user_balance", {userId});
   //      setBalance(response.data.balance);
 
   //      if (response.data.hasOwnProperty("balance")) {
